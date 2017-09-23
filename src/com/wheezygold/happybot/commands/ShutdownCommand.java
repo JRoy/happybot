5449efe5c8bc2e3bec5656461769a0a8ba532394
WheezyGold7931 <joshroy126@gmail.com>
2017-09-22 23:27:42 -0400

    Added 2 Shutdown Options (Closes #5)

--- src/com/wheezygold/happybot/commands/ShutdownCommand.java
+++ src/com/wheezygold/happybot/commands/ShutdownCommand.java
@@ -3,24 +3,41 @@ package com.wheezygold.happybot.commands;
 import com.jagrosh.jdautilities.commandclient.Command;
 import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import com.wheezygold.happybot.util.C;
+import com.wheezygold.happybot.util.Roles;
 
 public class ShutdownCommand extends Command {
 
     public ShutdownCommand()
     {
         this.name = "shutdown";
-        this.help = "Shutdowns the bot with love";
+        this.arguments = "<instance/vm>";
+        this.help = "Shutdowns the bot with love.";
         this.guildOnly = false;
-        this.ownerCommand = true;
         this.category = new Category("Bot Management");
     }
 
     @Override
     protected void execute(CommandEvent e) {
+        if (e.isOwner()) {
+            if (e.getArgs().startsWith("instance")) {
+                e.replySuccess(":white_check_mark: Stopping the JDA Instance!");
+                e.getJDA().shutdown();
+                C.log("The JDA instance has been shutdown.");
+            } else if (e.getArgs().startsWith("vm")) {
+                e.replySuccess(":white_check_mark: Stopping the VM!");
+                e.getJDA().shutdown();
+                C.log("The JDA instance has been shutdown...exiting the program.");
+                System.exit(0);
+            } else {
+                e.replyError("**Correct Usage:** ^" + name + " " + arguments);
+            }
+        } else {
+            e.replyError(C.permMsg(Roles.DEVELOPER));
+        }
         e.getMessage().addReaction("✅").queue();
-        e.getJDA().shutdown();
-        C.log("The JDA instance has been shutdown...exiting the program.");
-        System.exit(0);
+
+
+
     }
 
 }
