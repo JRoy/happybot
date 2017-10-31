24cd13f724376807c38f84c308bc8a00b941d7e8
WheezyGold7931 <joshroy126@gmail.com>
2017-10-31 16:36:21 -0400

    Convert YouTube API Calls to it's own class.

--- src/main/java/com/wheezygold/happybot/commands/StatsCommand.java
+++ src/main/java/com/wheezygold/happybot/commands/StatsCommand.java
@@ -4,6 +4,7 @@ import com.jagrosh.jdautilities.commandclient.Command;
 import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import com.wheezygold.happybot.util.C;
 import com.wheezygold.happybot.util.JSON;
+import com.wheezygold.happybot.util.YouTube;
 import net.dv8tion.jda.core.EmbedBuilder;
 import org.json.JSONObject;
 
@@ -32,28 +33,18 @@ public class StatsCommand extends Command {
 
         @Override
         public void run() {
-            JSONObject jsonObject = null;
-            try {
-                //Take the API Key idc, it's got nothing but youtube shit.
-                jsonObject = JSON.readJsonFromUrl("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC-enFKOrEf6N2Kq_YG3sFcQ&key=AIzaSyAPiPPTl1ZAsI1k_dCxHj7_RS7mfC-Dvuw");
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
 
-            JSONObject stats = JSON.readFromText(JSON.readFromText(jsonObject.getJSONArray("items").get(0).toString()).get("statistics").toString());
-
-            String subs = C.prettyNum(Integer.parseInt(stats.getString("subscriberCount")));
-            String vids = C.prettyNum(Integer.parseInt(stats.getString("videoCount")));
-            String views = C.prettyNum(Integer.parseInt(stats.getString("viewCount")));
+            YouTube youTube = new YouTube().pullAPI();
 
             e.reply(new EmbedBuilder()
                     .setTitle("**Happyheart's Statistics**")
                     .setDescription("Listing statistics:")
                     .setFooter("Stats provided by YouTube's Realtime API", "https://www.youtube.com/yts/img/favicon-vfl8qSV2F.ico")
-                    .addField("**Subscribers**", subs, true)
-                    .addField("**Videos**", vids, true)
-                    .addField("**Views**", views, true)
+                    .addField("**Subscribers**", youTube.fetchSubs(), true)
+                    .addField("**Videos**", youTube.fetchVids(), true)
+                    .addField("**Views**", youTube.fetchViews(), true)
                     .build());
+            youTube.finish();
         }
     }
 
