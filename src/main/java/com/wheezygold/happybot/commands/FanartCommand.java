ffb6decaf49a03527e12a15e8a3507660f46e84e
WheezyGold7931 <joshroy126@gmail.com>
2017-10-31 10:58:31 -0400

    Fix fanart command not dming' the target user. (Fixes #15)

--- src/main/java/com/wheezygold/happybot/commands/FanartCommand.java
+++ src/main/java/com/wheezygold/happybot/commands/FanartCommand.java
@@ -22,7 +22,7 @@ public class FanartCommand extends Command {
                 e.getMessage().delete().reason("Auto Command Deletion").queue();
                 String msg = C.getMentionedMember(e).getAsMention() + ", please post only fanart in " + e.getGuild().getTextChannelById("337689640888827905").getAsMention();
                 e.replySuccess(msg);
-                C.privChannel(e.getMember(), msg);
+                C.privChannel(C.getMentionedMember(e), msg);
             } else {
                 e.replyError("**Correct Usage:** ^" + name + " " + arguments);
             }
