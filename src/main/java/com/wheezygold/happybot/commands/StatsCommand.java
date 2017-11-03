724b4302cc5742d6c89f00754d9394077b48a9ed
WheezyGold7931 <joshroy126@gmail.com>
2017-11-02 22:05:46 -0400

    Add more use to ^stats

--- src/main/java/com/wheezygold/happybot/commands/StatsCommand.java
+++ src/main/java/com/wheezygold/happybot/commands/StatsCommand.java
@@ -2,32 +2,42 @@ package com.wheezygold.happybot.commands;
 
 import com.jagrosh.jdautilities.commandclient.Command;
 import com.jagrosh.jdautilities.commandclient.CommandEvent;
-import com.wheezygold.happybot.util.C;
-import com.wheezygold.happybot.util.JSON;
+import com.kbrewster.hypixelapi.exceptions.APIException;
+import com.kbrewster.hypixelapi.player.Player;
+import com.wheezygold.happybot.util.Hypixel;
 import com.wheezygold.happybot.util.YouTube;
 import net.dv8tion.jda.core.EmbedBuilder;
-import org.json.JSONObject;
 
-import java.io.IOException;
+import java.util.HashMap;
 
 public class StatsCommand extends Command {
 
-    public StatsCommand() {
+    private Hypixel hypixel;
+
+    public StatsCommand(Hypixel hypixel) {
         this.name = "stats";
-        this.help = "Gives stats of a happy's channel.";
+        this.help = "Gives stats of a happy's channel and hypixel player.";
+        this.arguments = "<youtube/hypixel>";
         this.category = new Category("Fun");
+        this.hypixel = hypixel;
     }
 
     @Override
     protected void execute(CommandEvent e) {
-        new Thread(new GetStats(e)).start();
+        if (e.getArgs().equalsIgnoreCase("youtube")) {
+            new Thread(new GetYoutubeStats(e)).start();
+        } else if (e.getArgs().equalsIgnoreCase("hypixel")) {
+            new Thread(new GetHypixelStats(e)).start();
+        } else {
+            e.replyError("**Correct Usage:** ^" + name + " " + arguments);
+        }
     }
 
-    class GetStats implements Runnable {
+    class GetYoutubeStats implements Runnable {
 
         private CommandEvent e;
 
-        public GetStats(CommandEvent e) {
+        GetYoutubeStats(CommandEvent e) {
             this.e = e;
         }
 
@@ -37,14 +47,46 @@ public class StatsCommand extends Command {
             YouTube youTube = new YouTube().pullAPI();
 
             e.reply(new EmbedBuilder()
-                    .setTitle("**Happyheart's Statistics**")
-                    .setDescription("Listing statistics:")
+                    .setTitle("Happyheart's YouTube Statistics")
+                    .setDescription("Listing Statistics:")
                     .setFooter("Stats provided by YouTube's Realtime API", "https://www.youtube.com/yts/img/favicon-vfl8qSV2F.ico")
                     .addField("**Subscribers**", youTube.fetchSubs(), true)
                     .addField("**Videos**", youTube.fetchVids(), true)
                     .addField("**Views**", youTube.fetchViews(), true)
                     .build());
             youTube.finish();
+        }
+    }
+
+    class GetHypixelStats implements Runnable {
+
+        private CommandEvent e;
+
+        GetHypixelStats(CommandEvent e) {
+            this.e = e;
+        }
+
+        @Override
+        public void run() {
+            Player hypixelPlayer = null;
+            try {
+                hypixelPlayer = hypixel.getPlayer("happyheart");
+            } catch (APIException e1) {
+                e1.printStackTrace();
+            }
+
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("Happyheart's Hypixel Statistics")
+                    .setDescription("Listing statistics:")
+                    .setFooter("Stats provided by Hypixel's API", "https://media-curse.cursecdn.com/attachments/264/727/f7c76fdb4569546a9ddf0e58c8653823.png");
+
+            for (HashMap.Entry<String, String> entry : Hypixel.getAllFields(hypixelPlayer).entrySet()) {
+                if (entry.getValue() != null) {
+                    embed.addField("**" + entry.getKey() + "**", entry.getValue(), true);
+                }
+            }
+
+            e.reply(embed.build());
         }
     }
 
