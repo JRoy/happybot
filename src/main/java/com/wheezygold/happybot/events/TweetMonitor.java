4b8af14dbc1d82cd815f70f2a829cf9f3b2b6316
WheezyGold7931 <joshroy126@gmail.com>
2017-11-07 17:10:21 -0500

    Fix tweet messages not tagging users (Fixes #25)

--- src/main/java/com/wheezygold/happybot/events/TweetMonitor.java
+++ src/main/java/com/wheezygold/happybot/events/TweetMonitor.java
@@ -8,6 +8,7 @@ import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class TweetMonitor {
     public TweetMonitor(String cKey, String cSecret, String aToken, String aSecret) {
@@ -22,21 +23,7 @@ public class TweetMonitor {
         StatusListener listener = new StatusListener() {
             @Override
             public void onStatus(Status status) {
-                Roles.TWITTER.getRole().getManager().setMentionable(true).queue();
-                EmbedBuilder builder = new EmbedBuilder()
-                        .setThumbnail(status.getUser().getBiggerProfileImageURL())
-                        .setTitle("@" + status.getUser().getScreenName() + " has just tweeted", "https://twitter.com/" + status.getUser().getScreenName() + "/status/" + status.getId())
-                        .setDescription(status.getText())
-                        .setColor(new Color(0, 172, 237));
-                if (status.getInReplyToScreenName() != null) {
-                    builder.addField("In reply to..", "[In reply to this @" + status.getInReplyToScreenName() + "'s tweet]" +
-                            "(https://twitter.com/" + status.getInReplyToScreenName() + "/status/" + status.getInReplyToStatusId() + ")", false);
-                }
-                if (status.getUser().getId() == happyid) {
-                    Channels.TWITTER.getChannel().sendMessage(Roles.TWITTER.getRole().getAsMention()).queue();
-                    Channels.TWITTER.getChannel().sendMessage(builder.build()).queue();
-                }
-                Roles.TWITTER.getRole().getManager().setMentionable(false).queue();
+                new Thread(new HandleTweet(status)).start();
             }
 
             @Override
@@ -69,4 +56,40 @@ public class TweetMonitor {
         filter.follow(happyid);
         twitterStream.filter(filter);
     }
+
+    private class HandleTweet implements Runnable {
+
+        private Status status;
+
+        HandleTweet(Status status) {
+            this.status = status;
+        }
+
+        @Override
+        public void run() {
+            Roles.TWITTER.getRole().getManager().setMentionable(true).queue();
+            EmbedBuilder builder = new EmbedBuilder()
+                    .setThumbnail(status.getUser().getBiggerProfileImageURL())
+                    .setTitle("@" + status.getUser().getScreenName() + " has just tweeted", "https://twitter.com/" + status.getUser().getScreenName() + "/status/" + status.getId())
+                    .setDescription(status.getText())
+                    .setColor(new Color(0, 172, 237));
+            if (status.getInReplyToScreenName() != null) {
+                builder.addField("In reply to..", "[In reply to this @" + status.getInReplyToScreenName() + "'s tweet]" +
+                        "(https://twitter.com/" + status.getInReplyToScreenName() + "/status/" + status.getInReplyToStatusId() + ")", false);
+            }
+            if (status.getUser().getId() == TwitterCentre.getHappyid()) {
+                Channels.TWITTER.getChannel().sendMessage(Roles.TWITTER.getRole().getAsMention()).queue();
+                Channels.TWITTER.getChannel().sendMessage(builder.build()).queue();
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                Roles.TWITTER.getRole().getManager().setMentionable(false).queue();
+            }
+        }
+
+    }
+
 }
