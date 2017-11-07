3f64fa3492a5bee585ae9138de36a5aaae1179bd
okx-code <me@oliverallen.co.uk>
2017-11-07 18:48:51 +0000

    remove bad code

--- src/main/java/com/wheezygold/happybot/sql/exceptions/NoCollectorRegistredException.java
+++
@@ -1,5 +0,0 @@
-package com.wheezygold.happybot.sql.exceptions;
-
-public class NoCollectorRegistredException extends RuntimeException {
-    private static final long serialVersionUID = -1915531568302229755L;
-}
--- src/main/java/com/wheezygold/happybot/sql/exceptions/NoTableInformedException.java
+++
@@ -1,5 +0,0 @@
-package com.wheezygold.happybot.sql.exceptions;
-
-public class NoTableInformedException extends RuntimeException {
-    private static final long serialVersionUID = -6714378944166481237L;
-}
