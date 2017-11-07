449ae1ecfa02f684b507441745d3805b2a5bdab5
WheezyGold7931 <joshroy126@gmail.com>
2017-11-07 18:15:06 -0500

    Add quick maths to ^math (Closes #21)

--- src/main/java/com/wheezygold/happybot/commands/MathCommand.java
+++ src/main/java/com/wheezygold/happybot/commands/MathCommand.java
@@ -19,10 +19,22 @@ public class MathCommand extends Command {
     protected void execute(CommandEvent e) {
 
         if (!e.getArgs().isEmpty()) {
-            BigDecimal result = null;
+            String  result = null;
+
+            if (e.getArgs().equalsIgnoreCase("quick maths")) {
+                result = "2 + 2 - 1 = that's 3 quick maths.";
+                e.replySuccess("**Expression Evaluated!**\n**Result:**" + C.codeblock(result));
+                return;
+            }
+
+            if (e.getArgs().replace(" ", "").equalsIgnoreCase("2+2-1")) {
+                result = "that's 3 quick maths";
+                e.replySuccess("**Expression Evaluated!**\n**Result:**" + C.codeblock(result));
+                return;
+            }
 
             try {
-                result = new Expression(e.getArgs()).eval();
+                result = new Expression(e.getArgs()).eval().toPlainString();
             } catch (Expression.ExpressionException | ArithmeticException e1) {
                 e.replyError("Invalid Expression!");
                 return;
@@ -33,7 +45,7 @@ public class MathCommand extends Command {
                 return;
             }
 
-            e.replySuccess("**Expression Evaluated!**\n**Result:**" + C.codeblock(result.toPlainString()));
+            e.replySuccess("**Expression Evaluated!**\n**Result:**" + C.codeblock(result));
         } else {
             e.replyError("**Correct Usage:** ^" + name + " " + arguments);
         }
