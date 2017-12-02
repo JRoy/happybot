117e721f609e12dcc457b7c9ffa410f58f2ff591
Techno-coder <zhiyuanqi@hotmail.co.nz>
2017-12-02 17:09:06 +1300

    (Fix #49) Fix star messages for non gilds

--- src/main/java/com/wheezygold/happybot/Main.java
+++ src/main/java/com/wheezygold/happybot/Main.java
@@ -11,6 +11,7 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.impl.JDAImpl;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.EventListener;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -23,265 +24,267 @@ import java.util.List;
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
 public class Main extends ListenerAdapter {
 
-    private Main instance = this;
-    private static JDA jda;
-    private static CommandClientBuilder clientBuilder;
-    private static String theme;
-    private static TwitterCentre twitterCentre;
-    private static TweetMonitor tweetMonitor;
-    private static SQLManager sqlManager;
-    private static Hypixel hypixel;
-    private static ThemeManager themeManager;
+	private Main instance = this;
+	private static JDA jda;
+	private static CommandClientBuilder clientBuilder;
+	private static String theme;
+	private static TwitterCentre twitterCentre;
+	private static TweetMonitor tweetMonitor;
+	private static SQLManager sqlManager;
+	private static Hypixel hypixel;
+	private static ThemeManager themeManager;
 
-    public static void main(String[] args) throws IOException, IllegalArgumentException, RateLimitedException, LoginException {
+	public static void main(
+			String[] args) throws IOException, IllegalArgumentException, RateLimitedException, LoginException {
 
-        new Logger();
+		new Logger();
 
-        Logger.info("Initializing the bot...");
+		Logger.info("Initializing the bot...");
 
-        Logger.log("Loading Config Files...");
-        createConfigFiles();
-
-
-        String token = readFirstLineOfFile("config.yml", "There is no token in your config, welcome to stack trace city!");
-        theme = readFirstLineOfFile("theme.yml", "Error receiving theme");
-
-        //Always init your strings! (Techno-coder: Wheezy, you are a sad, sad person)
-        String sqlPassword;
-        sqlPassword = readFirstLineOfFile("sql.yml", "Error receiving your SQL Password");
-
-        themeManager = loadThemeManager();
-
-        loadTweetMonitor();
-
-        //Load our SQL Stuff
-        sqlManager = new SQLManager(sqlPassword);
+		Logger.log("Loading Config Files...");
+		createConfigFiles();
 
 
-        Logger.info("Loading the Hypixel API...");
-        loadHypixelAPI();
+		String token = readFirstLineOfFile("config.yml",
+		                                   "There is no token in your config, welcome to stack trace city!");
+		theme = readFirstLineOfFile("theme.yml", "Error receiving theme");
 
-        List<EventListener> eventListeners = loadEventListeners();
-        loadClientBuilder();
+		//Always init your strings! (Techno-coder: Wheezy, you are a sad, sad person)
+		String sqlPassword;
+		sqlPassword = readFirstLineOfFile("sql.yml", "Error receiving your SQL Password");
 
-        Logger.info("Constructing the JDA Instance...");
-        JDABuilder builder = new JDABuilder(AccountType.BOT)
-                .setToken(token)
-                .setStatus(OnlineStatus.DO_NOT_DISTURB)
-                //Listens to the MessageReceivedEvent.
-                .addEventListener(clientBuilder.build())
-                .setGame(Game.of(Game.GameType.DEFAULT, "Loading"));
-        for (EventListener listener : eventListeners)
-            builder.addEventListener(listener);
-        jda = builder.buildAsync();
+		themeManager = loadThemeManager();
 
-//        new RichPresence((JDAImpl) jda);
+		loadTweetMonitor();
 
-        Logger.info("Bot has been loaded!");
-    }
-
-    private static ThemeManager loadThemeManager() {
-        return new ThemeManager();
-    }
-
-    private static List<EventListener> loadEventListeners() {
-        List<EventListener> eventListeners = new ArrayList<>();
-
-        Logger.info("Loading AutoMod...");
-        eventListeners.add(new AutoMod());
-
-        Logger.info("Loading Welcome Manager...");
-        eventListeners.add(new WelcomeMessage());
-
-        Logger.info("Loading AutoReact...");
-        eventListeners.add(new AutoReact());
-
-        Logger.info("Loading Message Starer...");
-        eventListeners.add(new StarMessages());
-
-        return eventListeners;
-    }
-
-    private static void loadClientBuilder() {
-        Logger.info("Loading the command builder...");
-
-        //Creates JDA-Util's Command Builder so we can use it later.
-        clientBuilder = new CommandClientBuilder();
-
-        //Used for "ownerOnly" commands in commands.
-        clientBuilder.setOwnerId(Constants.OWNER_ID.get());
-
-        //Used for the prefix of the bot, so we have an easy life.
-        clientBuilder.setPrefix("^");
-
-        Logger.info("Adding commands...");
-
-        //Loads all of our commands into JDA-Util's command handler.
-        clientBuilder.addCommands(
-
-                //General
-                new PingCommand(),
-                new RulesCommand(),
-                new ApplyCommand(),
-                new RewardsCommand(),
-                new MentionCommand(),
-                new ServersCommand(),
-                new SeasonCommand(),
-
-                //Fun
-
-                new HecktownCommand(),
-                new MathCommand(),
-                new VideoCommand(),
-                new RandomSeasonCommand(),
-                new StatsCommand(hypixel),
-                new HypixelCommand(hypixel),
-                new WelcomeStatsCommand(),
-                new MoneyCommand(sqlManager),
-                new GambleCommand(sqlManager),
-
-                //Staff Tools
-
-                new SpamCommand(),
-                new OgCommand(),
-                new FansCommand(),
-                new LockCommand(),
-                new UnlockCommand(),
-                new FanartCommand(),
-                new KickCommand(),
-                new BanCommand(),
-                new PardonCommand(),
-                new PromoteCommand(),
-                new DemoteCommand(),
-                new StaffManagementCommand(),
-
-                //Bot Management
-
-                new ThemeCommand(themeManager),
-                new ThemeManagerCommand(themeManager),
-                new ShutdownCommand(),
-                new UpdateCommand(),
-                new EvalCommand());
-
-        clientBuilder.setHelpFunction(C::showHelp);
-    }
-
-    private static void loadHypixelAPI() throws IOException {
-        String apiKey = null;
-        BufferedReader hypixelReader = new BufferedReader(new FileReader("hypixel.yml"));
-        try {
-            apiKey = hypixelReader.readLine();
-            hypixelReader.close();
-        } catch (NullPointerException hex) {
-            Logger.error("Error loading the Hypixel api-key: " + hex.getMessage());
-        }
-        hypixel = new Hypixel(apiKey);
-    }
-
-    private static void loadTweetMonitor() throws IOException {
-        String cKey = null;
-        String cSecret = null;
-        String  aToken = null;
-        String aSecret = null;
-        BufferedReader twitterReader = new BufferedReader(new FileReader("twitter.yml"));
-        try {
-            cKey = twitterReader.readLine();
-            cSecret = twitterReader.readLine();
-            aToken = twitterReader.readLine();
-            aSecret = twitterReader.readLine();
-            twitterReader.close();
-        } catch (NullPointerException tex) {
-            Logger.error("Error loading the Twitter credentials: " + tex.getMessage());
-        }
-
-        Logger.info("Loading Twitter Centre...");
-        twitterCentre = new TwitterCentre(cKey, cSecret, aToken, aSecret);
-        Logger.info("Loading Twitter Monitor...");
-        tweetMonitor = new TweetMonitor(cKey, cSecret, aToken, aSecret);
-    }
-
-    private static String readFirstLineOfFile(String filename, String errorMessage) throws IOException {
-        BufferedReader configReader = new BufferedReader(new FileReader(filename));
-        try {
-            String line = configReader.readLine();
-            configReader.close();
-            return line;
-        } catch (NullPointerException cex) {
-            Logger.error(errorMessage);
-        }
-        return "";
-    }
-
-    private static void createConfigFiles() throws IOException {
-        //Lets make the file so we can use it!
-        File configfile = createFile("config.yml");
-        File twitterfile = createFile("twitter.yml");
-        File sqlfile = createFile("sql.yml");
-        File themefile = new File("theme.yml");
-
-        //Create theme file with defaults if it does not exist
-        boolean themeFileNotExists = themefile.createNewFile();
-        if (themeFileNotExists) {
-            FileWriter fw = new FileWriter(themefile);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write("normal");
-            bw.close();
-            fw.close();
-        }
-    }
-
-    /**
-     * Creates a new file if it does not exist
-     *
-     * @param filename The filename for the file
-     * @return The reference to the open file
-     * @throws IOException If the file could not be created
-     */
-    private static File createFile(String filename) throws IOException {
-        File file = new File(filename);
-        boolean doesNotExist = file.createNewFile();
-        return file;
-    }
+//        Load our SQL Stuff
+		sqlManager = new SQLManager(sqlPassword);
 
 
-    /**
-     * An easy way to get our JDA Instance!
-     *
-     * @return Returns the JDA Instance.
-     */
-    public static JDA getJda() {
-        return jda;
-    }
+		Logger.info("Loading the Hypixel API...");
+		loadHypixelAPI();
 
-    /**
-     * An easy way to get our CommandClientBuilder instance!
-     *
-     * @return Returns the CommandClientBuilder Instance.
-     */
-    @SuppressWarnings("unused")
-    public static CommandClientBuilder getClientBuilder() {
-        return clientBuilder;
-    }
+		List<EventListener> eventListeners = loadEventListeners();
+		loadClientBuilder();
 
-    public static String getTheme() {
-        return theme;
-    }
+		Logger.info("Constructing the JDA Instance...");
+		JDABuilder builder = new JDABuilder(AccountType.BOT)
+				.setToken(token)
+				.setStatus(OnlineStatus.DO_NOT_DISTURB)
+				//Listens to the MessageReceivedEvent.
+				.addEventListener(clientBuilder.build())
+				.setGame(Game.of(Game.GameType.DEFAULT, "Loading"));
+		for (EventListener listener : eventListeners)
+			builder.addEventListener(listener);
+		jda = builder.buildAsync();
 
-    public static void updateTheme() {
-        try {
-            BufferedReader themeReader = new BufferedReader(new FileReader("theme.yml"));
-            //We will get the token just in case, I don't know, maybe we want to log in.
-            theme = themeReader.readLine();
-            Logger.info("Theme Loaded: " + theme + "!");
-            themeReader.close();
-        } catch (NullPointerException e) {
-            //Let them know they are going to die.
-            Logger.error("Error receiving your theme: " + e.getMessage());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            Logger.error("Error receiving your theme: " + e.getMessage());
-        }
-    }
+		new RichPresence((JDAImpl) jda);
+
+		Logger.info("Bot has been loaded!");
+	}
+
+	private static ThemeManager loadThemeManager() {
+		return new ThemeManager();
+	}
+
+	private static List<EventListener> loadEventListeners() {
+		List<EventListener> eventListeners = new ArrayList<>();
+
+		Logger.info("Loading AutoMod...");
+		eventListeners.add(new AutoMod());
+
+		Logger.info("Loading Welcome Manager...");
+		eventListeners.add(new WelcomeMessage());
+
+		Logger.info("Loading AutoReact...");
+		eventListeners.add(new AutoReact());
+
+		Logger.info("Loading Message Starer...");
+		eventListeners.add(new StarMessages());
+
+		return eventListeners;
+	}
+
+	private static void loadClientBuilder() {
+		Logger.info("Loading the command builder...");
+
+		//Creates JDA-Util's Command Builder so we can use it later.
+		clientBuilder = new CommandClientBuilder();
+
+		//Used for "ownerOnly" commands in commands.
+		clientBuilder.setOwnerId(Constants.OWNER_ID.get());
+
+		//Used for the prefix of the bot, so we have an easy life.
+		clientBuilder.setPrefix("^");
+
+		Logger.info("Adding commands...");
+
+		//Loads all of our commands into JDA-Util's command handler.
+		clientBuilder.addCommands(
+
+				//General
+				new PingCommand(),
+				new RulesCommand(),
+				new ApplyCommand(),
+				new RewardsCommand(),
+				new MentionCommand(),
+				new ServersCommand(),
+				new SeasonCommand(),
+
+				//Fun
+
+				new HecktownCommand(),
+				new MathCommand(),
+				new VideoCommand(),
+				new RandomSeasonCommand(),
+//                new StatsCommand(hypixel),
+//                new HypixelCommand(hypixel),
+				new WelcomeStatsCommand(),
+//                new MoneyCommand(sqlManager),
+//                new GambleCommand(sqlManager),
+
+				//Staff Tools
+
+				new SpamCommand(),
+				new OgCommand(),
+				new FansCommand(),
+				new LockCommand(),
+				new UnlockCommand(),
+				new FanartCommand(),
+				new KickCommand(),
+				new BanCommand(),
+				new PardonCommand(),
+				new PromoteCommand(),
+				new DemoteCommand(),
+				new StaffManagementCommand(),
+
+				//Bot Management
+
+				new ThemeCommand(themeManager),
+				new ThemeManagerCommand(themeManager),
+				new ShutdownCommand(),
+				new UpdateCommand(),
+				new EvalCommand());
+
+		clientBuilder.setHelpFunction(C::showHelp);
+	}
+
+	private static void loadHypixelAPI() throws IOException {
+		String apiKey = null;
+		BufferedReader hypixelReader = new BufferedReader(new FileReader("hypixel.yml"));
+		try {
+			apiKey = hypixelReader.readLine();
+			hypixelReader.close();
+		} catch (NullPointerException hex) {
+			Logger.error("Error loading the Hypixel api-key: " + hex.getMessage());
+		}
+		hypixel = new Hypixel(apiKey);
+	}
+
+	private static void loadTweetMonitor() throws IOException {
+		String cKey = null;
+		String cSecret = null;
+		String aToken = null;
+		String aSecret = null;
+		BufferedReader twitterReader = new BufferedReader(new FileReader("twitter.yml"));
+		try {
+			cKey = twitterReader.readLine();
+			cSecret = twitterReader.readLine();
+			aToken = twitterReader.readLine();
+			aSecret = twitterReader.readLine();
+			twitterReader.close();
+		} catch (NullPointerException tex) {
+			Logger.error("Error loading the Twitter credentials: " + tex.getMessage());
+		}
+
+		Logger.info("Loading Twitter Centre...");
+		twitterCentre = new TwitterCentre(cKey, cSecret, aToken, aSecret);
+		Logger.info("Loading Twitter Monitor...");
+		tweetMonitor = new TweetMonitor(cKey, cSecret, aToken, aSecret);
+	}
+
+	private static String readFirstLineOfFile(String filename, String errorMessage) throws IOException {
+		BufferedReader configReader = new BufferedReader(new FileReader(filename));
+		try {
+			String line = configReader.readLine();
+			configReader.close();
+			return line;
+		} catch (NullPointerException cex) {
+			Logger.error(errorMessage);
+		}
+		return "";
+	}
+
+	private static void createConfigFiles() throws IOException {
+		//Lets make the file so we can use it!
+		File configfile = createFile("config.yml");
+		File twitterfile = createFile("twitter.yml");
+		File sqlfile = createFile("sql.yml");
+		File themefile = new File("theme.yml");
+
+		//Create theme file with defaults if it does not exist
+		boolean themeFileNotExists = themefile.createNewFile();
+		if (themeFileNotExists) {
+			FileWriter fw = new FileWriter(themefile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("normal");
+			bw.close();
+			fw.close();
+		}
+	}
+
+	/**
+	 * Creates a new file if it does not exist
+	 *
+	 * @param filename The filename for the file
+	 * @return The reference to the open file
+	 * @throws IOException If the file could not be created
+	 */
+	private static File createFile(String filename) throws IOException {
+		File file = new File(filename);
+		boolean doesNotExist = file.createNewFile();
+		return file;
+	}
+
+
+	/**
+	 * An easy way to get our JDA Instance!
+	 *
+	 * @return Returns the JDA Instance.
+	 */
+	public static JDA getJda() {
+		return jda;
+	}
+
+	/**
+	 * An easy way to get our CommandClientBuilder instance!
+	 *
+	 * @return Returns the CommandClientBuilder Instance.
+	 */
+	@SuppressWarnings("unused")
+	public static CommandClientBuilder getClientBuilder() {
+		return clientBuilder;
+	}
+
+	public static String getTheme() {
+		return theme;
+	}
+
+	public static void updateTheme() {
+		try {
+			BufferedReader themeReader = new BufferedReader(new FileReader("theme.yml"));
+			//We will get the token just in case, I don't know, maybe we want to log in.
+			theme = themeReader.readLine();
+			Logger.info("Theme Loaded: " + theme + "!");
+			themeReader.close();
+		} catch (NullPointerException e) {
+			//Let them know they are going to die.
+			Logger.error("Error receiving your theme: " + e.getMessage());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			Logger.error("Error receiving your theme: " + e.getMessage());
+		}
+	}
 
 }
--- src/main/java/com/wheezygold/happybot/events/StarMessages.java
+++ src/main/java/com/wheezygold/happybot/events/StarMessages.java
@@ -2,6 +2,7 @@ package com.wheezygold.happybot.events;
 
 import com.wheezygold.happybot.util.C;
 import com.wheezygold.happybot.util.Channels;
+import com.wheezygold.happybot.util.Logger;
 import com.wheezygold.happybot.util.Roles;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
@@ -13,95 +14,105 @@ import java.util.HashSet;
 
 public class StarMessages extends ListenerAdapter {
 
-    private HashSet<String> alreadyUsedMessages = new HashSet<>();
+	private HashSet<String> alreadyUsedMessages = new HashSet<>();
 
-    @Override
-    public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent e) {
-        if (e.getReactionEmote().getName().equals("⭐")) {
-            e.getChannel().getMessageById(e.getMessageId()).queue(this::handleStar);
-        } else if (e.getReactionEmote().getName().equals("gild")) {
-            if (!C.hasRole(e.getMember(), Roles.MODERATOR)) {
-                e.getReaction().removeReaction().complete();
-                return;
-            }
-            e.getChannel().getMessageById(e.getMessageId()).queue(message -> {
-                if (e.getMember().getUser() == message.getAuthor()) {
-                    e.getReaction().removeReaction().complete();
-                    return;
-                }
-                handleGild(message);
-            });
-        }
-    }
+	@Override
+	public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent e) {
+		if (e.getReactionEmote().getName().equals("⭐")) {
+			e.getChannel().getMessageById(e.getMessageId()).queue(this::handleStar);
+		} else if (e.getReactionEmote().getName().equals("gild")) {
+			if (!C.hasRole(e.getMember(), Roles.MODERATOR)) {
+				e.getReaction().removeReaction().complete();
+				return;
+			}
+			e.getChannel().getMessageById(e.getMessageId()).queue(message -> {
+				if (e.getMember().getUser() == message.getAuthor()) {
+					e.getReaction().removeReaction().complete();
+					return;
+				}
+				handleGild(message);
+			});
+		}
+	}
 
-    private void handleStar(Message message) {
-        HandleStar handleStar = new HandleStar(message);
-        Thread t = new Thread(handleStar);
-        t.start();
-    }
+	private void handleStar(Message message) {
+		HandleStar handleStar = new HandleStar(message);
+		Thread t = new Thread(handleStar);
+		t.start();
+	}
 
-    private void handleGild(Message message) {
-        HandleGild handleGild = new HandleGild(message);
-        Thread t = new Thread(handleGild);
-        t.start();
-    }
+	private void handleGild(Message message) {
+		HandleGild handleGild = new HandleGild(message);
+		Thread t = new Thread(handleGild);
+		t.start();
+	}
 
-    private void sendStarredMessage(String footer, Message message, String privateMessageText) {
-        EmbedBuilder embed = new EmbedBuilder()
-                .setTitle(message.getMember().getEffectiveName())
-                .setDescription(message.getStrippedContent())
-                .setFooter(footer, "https://google.com")
-                .setThumbnail(message.getMember().getUser().getAvatarUrl())
-                .setColor(message.getMember().getColor());
-        Channels.STARRED_MESSAGES.getChannel().sendMessage(embed.build()).queue();
-        message.getAuthor().openPrivateChannel().queue(pc -> pc.sendMessage(privateMessageText).queue());
-        message.getAuthor().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(embed.build()).queue());
-        alreadyUsedMessages.add(message.getId());
-    }
+	private void sendStarredMessage(String footer, Message message, String privateMessageText) {
+		EmbedBuilder embed = new EmbedBuilder()
+				.setTitle(message.getMember().getEffectiveName())
+				.setDescription(message.getStrippedContent())
+				.setFooter(footer, "https://google.com")
+				.setThumbnail(message.getMember().getUser().getAvatarUrl())
+				.setColor(message.getMember().getColor());
+		Channels.STARRED_MESSAGES.getChannel().sendMessage(embed.build()).queue();
+		message.getAuthor().openPrivateChannel().queue(pc -> pc.sendMessage(privateMessageText).queue());
+		message.getAuthor().openPrivateChannel().queue(
+				privateChannel -> privateChannel.sendMessage(embed.build()).queue());
+		alreadyUsedMessages.add(message.getId());
+	}
 
-    private class HandleStar implements Runnable {
+	private class HandleStar implements Runnable {
 
-        private Message message;
-        private static final int NUM_STARS_REQUIRED = 5;
+		private Message message;
+		private static final int NUM_STARS_REQUIRED = 3;
 
-        HandleStar(Message message) {
-            this.message = message;
-        }
+		HandleStar(Message message) {
+			this.message = message;
+		}
 
-        @Override
-        public void run() {
-            if (message.getChannel().getId().equals(Channels.STARRED_MESSAGES.getId()) || message.getChannel().getId().equals(Channels.BOT_META.getId()) || message.getChannel().getId().equals(Channels.TWITTER.getId()) || message.getChannel().getId().equals(Channels.LIVE.getId())) {
-                return;
-            }
-            int numberOfStars = 0;
-            for (MessageReaction reaction : message.getReactions()) {
-                if (reaction.getEmote().getName().equals("⭐")) numberOfStars++;
-            }
-            if (numberOfStars == NUM_STARS_REQUIRED && !alreadyUsedMessages.contains(message.getId())) {
-                String footer = "New Starred Message from #" + message.getChannel().getName();
-                String privateMessageText = "Congrats! One of your messages has been stared:";
-                sendStarredMessage(footer, message, privateMessageText);
-            }
-        }
+		@Override
+		public void run() {
+			if (message.getChannel().getId().equals(
+					Channels.STARRED_MESSAGES.getId()) || message.getChannel().getId().equals(
+					Channels.BOT_META.getId()) || message.getChannel().getId().equals(
+					Channels.TWITTER.getId()) || message.getChannel().getId().equals(Channels.LIVE.getId())) {
+				return;
+			}
 
-    }
+			try {
+				//noinspection ConstantConditions
+				int numberOfStars = message.getReactions().stream()
+						.filter(reaction -> reaction.getReactionEmote().getName().equals("⭐"))
+						.findAny().orElse(null).getCount();
 
-    private class HandleGild implements Runnable {
+				if (numberOfStars == NUM_STARS_REQUIRED && !alreadyUsedMessages.contains(message.getId())) {
+					String footer = "New Starred Message from #" + message.getChannel().getName();
+					String privateMessageText = "Congrats! One of your messages has been stared:";
+					sendStarredMessage(footer, message, privateMessageText);
+				}
+			} catch (NullPointerException | IllegalStateException e) {
+				Logger.error("Star reaction is in invalid state!");
+			}
+		}
 
-        private Message message;
+	}
 
-        HandleGild(Message message) {
-            this.message = message;
-        }
+	private class HandleGild implements Runnable {
 
-        @Override
-        public void run() {
-            if (!alreadyUsedMessages.contains(message.getId())) {
-                String footer = "New Gilded Message from #" + message.getChannel().getName();
-                String privateMessageText = "Congrats! One of your messages has been gilded by a staff member:";
-                sendStarredMessage(footer, message, privateMessageText);
-            }
+		private Message message;
 
-        }
-    }
+		HandleGild(Message message) {
+			this.message = message;
+		}
+
+		@Override
+		public void run() {
+			if (!alreadyUsedMessages.contains(message.getId())) {
+				String footer = "New Gilded Message from #" + message.getChannel().getName();
+				String privateMessageText = "Congrats! One of your messages has been gilded by a staff member:";
+				sendStarredMessage(footer, message, privateMessageText);
+			}
+
+		}
+	}
 }
