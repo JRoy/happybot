091b7bd80ba2f00996ad1d15b73bdff929dd7749
WheezyGold7931 <joshroy126@gmail.com>
2018-01-13 10:02:12 -0500

    Add reasons for kick and ban commands.

--- src/main/java/com/wheezygold/happybot/commands/BanCommand.java
+++ src/main/java/com/wheezygold/happybot/commands/BanCommand.java
@@ -4,12 +4,14 @@ import com.jagrosh.jdautilities.commandclient.Command;
 import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import com.wheezygold.happybot.util.C;
 import com.wheezygold.happybot.util.Roles;
+import net.dv8tion.jda.core.entities.Member;
 
 public class BanCommand extends Command {
     public BanCommand() {
         this.name = "ban";
+        this.aliases = new String[]{"begone"};
         this.help = "Bans target user from server.";
-        this.arguments = "<user mention>";
+        this.arguments = "<user mention> <reason>";
         this.guildOnly = true;
         this.category = new Category("Staff Tools");
     }
@@ -17,11 +19,15 @@ public class BanCommand extends Command {
     @Override
     protected void execute(CommandEvent e) {
         if (C.hasRole(e.getMember(), Roles.MODERATOR)) {
-            if (e.getMessage().getMentionedUsers().size() == 1) {
-                String bname = C.getMentionedMember(e).getUser().getName();
-                String bdescrim = C.getMentionedMember(e).getUser().getDiscriminator();
-                C.getCtrl(e).ban(C.getMentionedMember(e).getUser(), 7, "Banned by Moderator: " + e.getMember().getUser().getName()).reason("Banned by Moderator: " + e.getMember().getUser().getName()).queue();
-                e.replySuccess("User " + bname + "#" + bdescrim + " has been **FRIGGING BANNED** by " + e.getMember().getEffectiveName());
+            if (C.containsMention(e)) {
+                if (e.getArgs().replaceAll("<(.*?)>", "").isEmpty()) {
+                    e.replyError("**Correct Usage:** ^" + name + " " + arguments);
+                    return;
+                }
+                String reason = e.getArgs().replaceFirst("<(.*?)> ", "");
+                Member target = C.getMentionedMember(e);
+                C.getCtrl(e).ban(target.getUser(), 7, "Banned by Moderator: " + e.getMember().getUser().getName()).reason("Banned by Moderator: " + e.getMember().getUser().getName() + ". With Reason: " + reason).queue();
+                e.replySuccess("User " + target.getUser().getName() + "#" + target.getUser().getDiscriminator() + " has been **FRIGGING BANNED** by " + e.getMember().getEffectiveName());
             } else {
                 e.replyError("**Correct Usage:** ^" + name + " " + arguments);
             }
--- src/main/java/com/wheezygold/happybot/commands/KickCommand.java
+++ src/main/java/com/wheezygold/happybot/commands/KickCommand.java
@@ -4,12 +4,13 @@ import com.jagrosh.jdautilities.commandclient.Command;
 import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import com.wheezygold.happybot.util.C;
 import com.wheezygold.happybot.util.Roles;
+import net.dv8tion.jda.core.entities.Member;
 
 public class KickCommand extends Command {
     public KickCommand() {
         this.name = "kick";
         this.help = "Kicks target user from server.";
-        this.arguments = "<user mention>";
+        this.arguments = "<user mention> <reason>";
         this.guildOnly = true;
         this.category = new Category("Staff Tools");
         this.aliases = new String[]{"deport"};
@@ -18,11 +19,15 @@ public class KickCommand extends Command {
     @Override
     protected void execute(CommandEvent e) {
         if (C.hasRole(e.getMember(), Roles.HELPER)) {
-            if (e.getMessage().getMentionedUsers().size() == 1) {
-                String bname = C.getMentionedMember(e).getUser().getName();
-                String bdescrim = C.getMentionedMember(e).getUser().getDiscriminator();
-                C.getCtrl(e).kick(C.getMentionedMember(e)).reason("Banned by Moderator: " + e.getMember().getUser().getName()).queue();
-                e.replySuccess("User " + bname + "#" + bdescrim + " has been **FLIPPIN KICKED** by " + e.getMember().getEffectiveName());
+            if (C.containsMention(e)) {
+                if (e.getArgs().replaceAll("<(.*?)>", "").isEmpty()) {
+                    e.replyError("**Correct Usage:** ^" + name + " " + arguments);
+                    return;
+                }
+                String reason = e.getArgs().replaceFirst("<(.*?)> ", "");
+                Member target = C.getMentionedMember(e);
+                C.getCtrl(e).kick(target).reason("Kicked by Moderator: " + e.getMember().getUser().getName() + ". With Reason: " + reason).queue();
+                e.replySuccess("User " + target.getUser().getName() + "#" + target.getUser().getDiscriminator() + " has been **FLIPPIN KICKED** by " + e.getMember().getEffectiveName());
             } else {
                 e.replyError("**Correct Usage:** ^" + name + " " + arguments);
             }
