76ca2db2b7695cd8549c5d95ca45cb849eb92979
JRoy <joshroy126@gmail.com>
2018-06-06 23:17:01 -0400

    Convert ^fixfans to CommandBase

--- src/main/java/io/github/jroy/happybot/commands/FansCommand.java
+++ src/main/java/io/github/jroy/happybot/commands/FansCommand.java
@@ -1,34 +1,28 @@
 package io.github.jroy.happybot.commands;
 
-import com.jagrosh.jdautilities.command.Command;
-import com.jagrosh.jdautilities.command.CommandEvent;
+import io.github.jroy.happybot.commands.base.CommandBase;
+import io.github.jroy.happybot.commands.base.CommandCategory;
+import io.github.jroy.happybot.commands.base.CommandEvent;
 import io.github.jroy.happybot.util.C;
 import io.github.jroy.happybot.util.Roles;
 import net.dv8tion.jda.core.entities.Member;
 
-public class FansCommand extends Command {
+public class FansCommand extends CommandBase {
 
     public FansCommand() {
-        this.name = "fixfans";
-        this.help = "Fixes broken users with no Fans Role";
-        this.guildOnly = true;
-        this.category = new Category("Staff Tools");
+        super("fixfans", null, "Fixes broken users with no Fans Role", CommandCategory.STAFF, Roles.SUPER_ADMIN);
     }
 
     @Override
-    protected void execute(CommandEvent e) {
-        if (C.hasRole(e.getMember(), Roles.SUPER_ADMIN)) {
-            e.reply("Loading...");
-            int affected = 0;
-            for (Member curM : C.getGuild().getMembers()) {
-                if (!C.hasRole(curM, Roles.FANS) && !curM.getUser().isBot()) {
-                    C.giveRole(curM, Roles.FANS);
-                    affected++;
-                }
+    protected void executeCommand(CommandEvent e) {
+        e.reply("Loading...");
+        int affected = 0;
+        for (Member curM : C.getGuild().getMembers()) {
+            if (!C.hasRole(curM, Roles.FANS) && !curM.getUser().isBot()) {
+                C.giveRole(curM, Roles.FANS);
+                affected++;
             }
-            e.replySuccess("All Done!\n" + String.valueOf(affected) + " Users Affected!");
-        } else {
-            e.replyError(C.permMsg(Roles.SUPER_ADMIN));
         }
+        e.replySuccess("All Done!\n" + String.valueOf(affected) + " Users Affected!");
     }
 }
