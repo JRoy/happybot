02f5fa2aa6131b91e25c17c1f8ca1abf0468a8b0
JRoy <joshroy126@gmail.com>
2018-07-07 14:47:39 -0400

    Add one-command help

--- src/main/java/io/github/jroy/happybot/commands/HelpCommand.java
+++ src/main/java/io/github/jroy/happybot/commands/HelpCommand.java
@@ -19,14 +19,24 @@ public class HelpCommand extends CommandBase {
 
     @Override
     protected void executeCommand(CommandEvent e) {
+        if (!e.getArgs().isEmpty()) {
+            String cmd = e.getArgs().replace("^", "");
+            if (commandFactory.getRegisteredCommands().containsKey(cmd)) {
+                CommandBase base = commandFactory.getRegisteredCommands().get(cmd);
+                e.reply("Command Help:\n" +
+                        "`^" + base.getName() + (base.getArguments() == null ? "` - " : " " + base.getArguments() + "` - ") + base.getHelp() + (base.getPermissionRole() == null ? " (Public Command)" : " (Requires: " + base.getPermissionRole().getRoleName() + ")") + "\n");
+                return;
+            } else {
+                e.reply("Invalid command!");
+                return;
+            }
+        }
 
         Map<CommandCategory, List<CommandBase>> catBase = commandFactory.getCategorizedCommands();
-
         e.replySuccess("Help is on the way! :sparkles:");
         StringBuilder builder = new StringBuilder("**happybot** commands:\n\n");
 
         //Infer Categories
-
         for (CommandCategory curCat : CommandCategory.values()) {
             builder.append("**__").append(curCat.toString()).append("__**\n\n");
             for (CommandBase base : catBase.get(curCat))
