a6901c44f393717e40cbff3ddc37107686013b2a
JRoy <joshroy126@gmail.com>
2018-07-13 10:30:08 -0400

    Limit robbing the same user to once a day

--- src/main/java/io/github/jroy/happybot/commands/money/RobCommand.java
+++ src/main/java/io/github/jroy/happybot/commands/money/RobCommand.java
@@ -9,12 +9,16 @@ import net.dv8tion.jda.core.entities.Member;
 
 import java.sql.SQLException;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RobCommand extends CommandBase {
+
     private final static int FINE = 300;
 
     private SQLManager sqlManager;
+
+    private final HashMap<String, RobToken> robTokens = new HashMap<>();
 
     public RobCommand(SQLManager sqlManager) {
         super("rob", "<user>", "Robs a user for their money.", CommandCategory.FUN);
@@ -32,21 +36,36 @@ public class RobCommand extends CommandBase {
             }
 
             String userId = e.getMember().getUser().getId();
-            Member target = e.getMentionedMember();
-            if (!sqlManager.isActiveUserH(userId) || !sqlManager.isActiveUserH(target.getUser().getId())) {
+            Member targetMember = e.getMentionedMember();
+            if (!sqlManager.isActiveUserH(userId) || !sqlManager.isActiveUserH(targetMember.getUser().getId())) {
                 e.reply("Both you and the person you are attempting to rob must have a money account!");
                 removeFromCooldown(e.getMember());
                 return;
             }
-
-            if (userId.equalsIgnoreCase(target.getUser().getId())) {
+            if (userId.equalsIgnoreCase(targetMember.getUser().getId())) {
                 e.reply("Why would you rob yourself?");
                 removeFromCooldown(e.getMember());
                 return;
             }
 
+            if (!robTokens.containsKey(userId)) {
+                robTokens.put(userId, new RobToken());
+            }
+            RobToken token = robTokens.get(userId);
+            if (!token.canRob(targetMember.getUser().getId())) {
+                e.replyError("You may only rob a user once a day!");
+                removeFromCooldown(e.getMember());
+                return;
+            }
+
             UserToken userToken = sqlManager.getUser(userId);
-            UserToken targetToken = sqlManager.getUser(target.getUser().getId());
+            UserToken targetToken = sqlManager.getUser(targetMember.getUser().getId());
+
+            if (targetToken.getCoins() < 1000) {
+                e.reply("This person has under 1000 coins, give them a break!");
+                removeFromCooldown(e.getMember());
+                return;
+            }
 
             int robAmount = ThreadLocalRandom.current().nextInt(100, 500);
             if (targetToken.getCoins() < robAmount) {
@@ -60,24 +79,20 @@ public class RobCommand extends CommandBase {
                 return;
             }
 
-            if (targetToken.getCoins() < 1000) {
-                e.reply("This person has under 1000 coins, give them a break!");
-                removeFromCooldown(e.getMember());
-                return;
-            }
-
             double chance = Math.random();
             if (chance < 0.5) {
                 targetToken.takeCoins(robAmount);
                 userToken.addCoins(robAmount);
-                e.reply("Hey " + target.getAsMention() + ", you just got robbed by " + e.getMember().getAsMention() + " for " + robAmount + " coins!");
+                e.reply("Hey " + targetMember.getAsMention() + ", you just got robbed by " + e.getMember().getAsMention() + " for " + robAmount + " coins!");
             } else {
                 e.reply(e.getMember().getAsMention() + ", the feds caught you in the act you thief.\n" +
                     "    -" + FINE + " coins as fine.");
                 if (userToken.getCoins() >= FINE) {
                     userToken.takeCoins(FINE);
+                    token.registerRob(targetMember.getUser().getId());
                     return;
                 }
+                token.registerRob(targetMember.getUser().getId());
                 userToken.takeCoins(userToken.getCoins());
             }
         } catch (SQLException e1) {
---
+++ src/main/java/io/github/jroy/happybot/commands/money/RobToken.java
@@ -0,0 +1,32 @@
+package io.github.jroy.happybot.commands.money;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+
+@SuppressWarnings("FieldCanBeLocal")
+class RobToken {
+
+    /**
+     * Robbed ID & Time Until Can Rob Again
+     */
+    private final HashMap<String, OffsetDateTime> dailyTimes = new HashMap<>();
+    private final int ROB_DELAY = 86400000; //1 rob per day per person
+
+    RobToken() {
+
+    }
+
+    void registerRob(String userId) {
+        dailyTimes.put(userId, OffsetDateTime.now().plusSeconds(ROB_DELAY));
+    }
+
+    boolean canRob(String userId) {
+        return !dailyTimes.containsKey(userId) || getTimeRemainingForUser(userId) <= 0;
+    }
+
+    private int getTimeRemainingForUser(String userId) {
+        return (int) OffsetDateTime.now().until(dailyTimes.get(userId), ChronoUnit.SECONDS);
+    }
+
+}
