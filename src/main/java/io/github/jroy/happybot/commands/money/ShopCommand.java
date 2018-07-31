5c876cfd076c4b39520481d84b37a2ee7b45271d
JRoy <joshroy126@gmail.com>
2018-07-31 11:54:08 -0400

    Add ^shop reclaim

--- src/main/java/io/github/jroy/happybot/commands/money/ShopCommand.java
+++ src/main/java/io/github/jroy/happybot/commands/money/ShopCommand.java
@@ -11,78 +11,93 @@ import net.dv8tion.jda.core.EmbedBuilder;
 import org.apache.commons.lang3.StringUtils;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class ShopCommand extends CommandBase {
   private static final String CURRENT_SHOP_HELP = "**Happyheart Shop Help**\n" +
       "This shop allows for you to spend your money on\n" +
       "stuff. To view the products we offer please do\n" +
       "`^shop items` to get the list of the shop items.\n" +
+      "`^shop reclaim` lets you reclaim items you bought.\n" +
       "If you would like to buy an item do the following:\n" +
       "`^shop buy <id>` This will buy it from your account!";
 
   private PurchaseManager purchaseManager;
 
 
-    public ShopCommand(PurchaseManager purchaseManager) {
-        super("shop", "<page/buy/help>", "Fun activity thing let's you do things.", CommandCategory.FUN);
-        this.purchaseManager = purchaseManager;
+  public ShopCommand(PurchaseManager purchaseManager) {
+    super("shop", "<page/buy/help>", "Fun activity thing let's you do things.", CommandCategory.FUN);
+    this.purchaseManager = purchaseManager;
+  }
+
+  @Override
+  protected void executeCommand(CommandEvent e) {
+    if (e.getArgs().isEmpty()) {
+      e.replyError(CURRENT_SHOP_HELP);
+      return;
     }
-
-    @Override
-    protected void executeCommand(CommandEvent e) {
-        if (e.getArgs().isEmpty()) {
-            e.replyError(CURRENT_SHOP_HELP);
-            return;
+    if (e.getArgs().equalsIgnoreCase("items")) {
+      EmbedBuilder builder = new EmbedBuilder()
+          .setTitle("Coin Shop")
+          .setDescription("Here are the items for sale in the shop at this point in time.");
+      for (Reward reward : Reward.values()) {
+        builder.addField("#" + reward.getId() + " " + reward.getDisplay(), C.prettyNum(reward.getAmount()), false);
+      }
+      e.reply(builder.build());
+    } else if (e.getArgs().equalsIgnoreCase("reclaim")) {
+      try {
+        List<Reward> rewards = purchaseManager.getAllRewardsList(e.getMember().getUser().getId());
+        int amount = 0;
+        for (Reward reward : rewards) {
+          reward.getReward().processReward(e);
+          amount++;
         }
-        if (e.getArgs().equalsIgnoreCase("items")) {
-            EmbedBuilder builder = new EmbedBuilder()
-                    .setTitle("Coin Shop")
-                    .setDescription("Here are the items for sale in the shop at this point in time.");
-            for (Reward reward : Reward.values()) {
-                builder.addField("#" + reward.getId() + " " + reward.getDisplay(), C.prettyNum(reward.getAmount()), false);
-            }
-            e.reply(builder.build());
-        } else if (e.getArgs().startsWith("buy")) {
-            String id = e.getArgs().replaceFirst("buy ", "");
-            if (!StringUtils.isNumeric(id)) {
-                e.replyError(C.bold("Correct Usage:") + " ^shop buy **<id>**");
-                return;
-            }
-            int selectedID = Integer.parseInt(id);
-            if (!Reward.containsID(selectedID)) {
-                e.replyError(C.bold("Correct Usage:") + " ^shop buy **<id>**");
-                return;
-            }
+        e.reply("Reclaimed " + amount + " items!");
+      } catch (SQLException e1) {
+        e.replyError("Whoops");
+        e1.printStackTrace();
+      }
+    } else if (e.getArgs().startsWith("buy")) {
+      String id = e.getArgs().replaceFirst("buy ", "");
+      if (!StringUtils.isNumeric(id)) {
+        e.replyError(C.bold("Correct Usage:") + " ^shop buy **<id>**");
+        return;
+      }
+      int selectedID = Integer.parseInt(id);
+      if (!Reward.containsID(selectedID)) {
+        e.replyError(C.bold("Correct Usage:") + " ^shop buy **<id>**");
+        return;
+      }
 
-            //Grab Desired Item
-            Reward reward = Reward.getFromId(selectedID);
+      //Grab Desired Item
+      Reward reward = Reward.getFromId(selectedID);
 
-            if (reward == null) {
-                e.replyError("There was an error while handling your purchase. You have not been charged. [Null Reward]");
-                return;
-            }
+      if (reward == null) {
+        e.replyError("There was an error while handling your purchase. You have not been charged. [Null Reward]");
+        return;
+      }
 
-            if (!purchaseManager.getSqlManager().isActiveUserH(e.getMember().getUser().getId())) {
-                e.replyError(MoneyCommand.NEED_ACCOUNT);
-                return;
-            }
+      if (!purchaseManager.getSqlManager().isActiveUserH(e.getMember().getUser().getId())) {
+        e.replyError(MoneyCommand.NEED_ACCOUNT);
+        return;
+      }
 
-            try {
-                UserToken userToken = purchaseManager.getSqlManager().getUser(e.getMember().getUser().getId());
-                if (purchaseManager.hasReward(e.getMember().getUser().getId(), reward)) {
-                    e.replyError("You already own this item!");
-                    return;
-                }
-                if (userToken.getCoins() < reward.getAmount()) {
-                    e.replyError("You do not have the required funds to complete this purchase!");
-                    return;
-                }
-                reward.getReward().buyReward(userToken, purchaseManager, e, reward);
-            } catch (SQLException e1) {
-                e.replyError("There was an error while handling your purchase. You have not been charged. [SQL Error]");
-            }
-
-
+      try {
+        UserToken userToken = purchaseManager.getSqlManager().getUser(e.getMember().getUser().getId());
+        if (purchaseManager.hasReward(e.getMember().getUser().getId(), reward)) {
+          e.replyError("You already own this item!");
+          return;
         }
+        if (userToken.getCoins() < reward.getAmount()) {
+          e.replyError("You do not have the required funds to complete this purchase!");
+          return;
+        }
+        reward.getReward().buyReward(userToken, purchaseManager, e, reward);
+      } catch (SQLException e1) {
+        e.replyError("There was an error while handling your purchase. You have not been charged. [SQL Error]");
+      }
+
+
     }
+  }
 }
--- src/main/java/io/github/jroy/happybot/sql/PurchaseManager.java
+++ src/main/java/io/github/jroy/happybot/sql/PurchaseManager.java
@@ -5,71 +5,84 @@ import io.github.jroy.happybot.util.Logger;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class PurchaseManager {
 
-    private final SQLManager sqlManager;
+  private final SQLManager sqlManager;
 
-    private final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `purchases` ( `id` INT(50) NOT NULL AUTO_INCREMENT , `userId` VARCHAR(255) NOT NULL , `itemId` INT(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
-    private final String SELECT_ALL_REWARDS_FROM_USER = "SELECT * FROM `purchases` WHERE userId = ?;";
-    private final String SELECT_REWARD_FROM_USER = "SELECT * FROM `purchases` WHERE userId = ? AND itemId = ?;";
-    private final String ADD_REWARD = "INSERT INTO `purchases` (userId, itemId) VALUES (?, ?);";
-    private final String DELETE_REWARD = "DELETE FROM `purchases` WHERE userId = ? and itemId = ? LIMIT 1;";
+  private final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `purchases` ( `id` INT(50) NOT NULL AUTO_INCREMENT , `userId` VARCHAR(255) NOT NULL , `itemId` INT(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+  private final String SELECT_ALL_REWARDS_FROM_USER = "SELECT * FROM `purchases` WHERE userId = ?;";
+  private final String SELECT_REWARD_FROM_USER = "SELECT * FROM `purchases` WHERE userId = ? AND itemId = ?;";
+  private final String ADD_REWARD = "INSERT INTO `purchases` (userId, itemId) VALUES (?, ?);";
+  private final String DELETE_REWARD = "DELETE FROM `purchases` WHERE userId = ? and itemId = ? LIMIT 1;";
 
-    public PurchaseManager(SQLManager sqlManager) {
-        Logger.info("Loading Purchase Manger...");
-        this.sqlManager = sqlManager;
-        try {
-            sqlManager.getConnection().createStatement().execute(CREATE_TABLE);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Logger.error("Error while creating table: " + e.getMessage());
-        }
-        Logger.info("Loaded Purchase Manager!");
+  public PurchaseManager(SQLManager sqlManager) {
+    Logger.info("Loading Purchase Manger...");
+    this.sqlManager = sqlManager;
+    try {
+      sqlManager.getConnection().createStatement().execute(CREATE_TABLE);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      Logger.error("Error while creating table: " + e.getMessage());
     }
+    Logger.info("Loaded Purchase Manager!");
+  }
 
-    public boolean hasReward(String userId, Reward reward) {
-        try {
-            PreparedStatement statement = sqlManager.getConnection().prepareStatement(SELECT_REWARD_FROM_USER);
-            statement.setString(1, userId);
-            statement.setInt(2, reward.getId());
-            return statement.executeQuery().next();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+  public boolean hasReward(String userId, Reward reward) {
+    try {
+      PreparedStatement statement = sqlManager.getConnection().prepareStatement(SELECT_REWARD_FROM_USER);
+      statement.setString(1, userId);
+      statement.setInt(2, reward.getId());
+      return statement.executeQuery().next();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
     }
+  }
 
-    public ResultSet getAllRewards(String userId) throws SQLException {
-        PreparedStatement statement = sqlManager.getConnection().prepareStatement(SELECT_ALL_REWARDS_FROM_USER);
-        statement.setString(1, userId);
-        return statement.executeQuery();
-    }
+  public ResultSet getAllRewards(String userId) throws SQLException {
+    PreparedStatement statement = sqlManager.getConnection().prepareStatement(SELECT_ALL_REWARDS_FROM_USER);
+    statement.setString(1, userId);
+    return statement.executeQuery();
+  }
 
-    public void addReward(String userId, Reward reward) {
-        try {
-            PreparedStatement statement = sqlManager.getConnection().prepareStatement(ADD_REWARD);
-            statement.setString(1, userId);
-            statement.setInt(2, reward.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+  public List<Reward> getAllRewardsList(String userId) throws SQLException {
+    PreparedStatement statement = sqlManager.getConnection().prepareStatement(SELECT_ALL_REWARDS_FROM_USER);
+    statement.setString(1, userId);
+    ResultSet set = statement.executeQuery();
+    List<Reward> rewards = new ArrayList<>();
+    while (set.next()) {
+      rewards.add(Reward.getFromId(set.getInt("itemId")));
     }
+    return rewards;
+  }
 
-    public void deleteSingleReward(String userId, Reward reward) {
-        try {
-            PreparedStatement statement = sqlManager.getConnection().prepareStatement(DELETE_REWARD);
-            statement.setString(1, userId);
-            statement.setInt(2, reward.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+  public void addReward(String userId, Reward reward) {
+    try {
+      PreparedStatement statement = sqlManager.getConnection().prepareStatement(ADD_REWARD);
+      statement.setString(1, userId);
+      statement.setInt(2, reward.getId());
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+  }
 
-    public SQLManager getSqlManager() {
-        return sqlManager;
+  public void deleteSingleReward(String userId, Reward reward) {
+    try {
+      PreparedStatement statement = sqlManager.getConnection().prepareStatement(DELETE_REWARD);
+      statement.setString(1, userId);
+      statement.setInt(2, reward.getId());
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+  }
+
+  public SQLManager getSqlManager() {
+    return sqlManager;
+  }
 }
