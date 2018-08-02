64aa8434157bf06ba71c1e2a847ebaa4605597cb
JRoy <joshroy126@gmail.com>
2018-08-02 12:58:35 -0400

    Add Safe Rest Actions which prevent client side ghosting

---
+++ src/main/java/io/github/jroy/happybot/util/SafeRestAction.java
@@ -0,0 +1,78 @@
+package io.github.jroy.happybot.util;
+
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.requests.RestAction;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+public class SafeRestAction {
+
+  private final RestAction restAction;
+
+  private TimeUnit timeUnit = null;
+  private Long time = null;
+
+  public SafeRestAction(RestAction restAction) {
+    this.restAction = restAction;
+  }
+
+  public SafeRestAction wait(TimeUnit timeUnit, long time) {
+    this.timeUnit = timeUnit;
+    this.time = time;
+    return this;
+  }
+
+  public void queue() {
+    queue(null);
+  }
+
+  public void queue(@Nullable Consumer<Message> consumer) {
+    if (timeUnit == null || time == null) {
+      restAction.queue();
+      return;
+    }
+    new Thread(new ImpendAction(this, consumer)).start();
+  }
+
+
+  private Long getTime() {
+    return time;
+  }
+
+  private TimeUnit getTimeUnit() {
+    return timeUnit;
+  }
+
+  private RestAction getRestAction() {
+    return restAction;
+  }
+
+  private class ImpendAction implements Runnable {
+
+    private final SafeRestAction safeRestAction;
+    private final Consumer consumer;
+
+    ImpendAction(SafeRestAction safeRestAction, @Nullable Consumer<Message> consumer) {
+      this.safeRestAction = safeRestAction;
+      this.consumer = consumer;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void run() {
+      try {
+        safeRestAction.getTimeUnit().sleep(safeRestAction.getTime());
+        if (consumer == null) {
+          safeRestAction.getRestAction().queue();
+        } else {
+          safeRestAction.getRestAction().queue(consumer);
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+}
