00d4578555bde3d6e2e76250e89f958a567e0afd
JRoy <joshroy126@gmail.com>
2018-09-08 00:19:40 -0400

    Update boot script

--- bot.sh
+++ bot.sh
@@ -4,6 +4,9 @@ DROPBOX_URL=*censored*
 JENKINS_URL=http://127.0.0.1:8080/job/happybot//lastSuccessfulBuild/artifact/build/libs/happybot-all.jar
 JENKINS_DEV_URL=http://127.0.0.1:8080/job/happybot-dev//lastSuccessfulBuild/artifact/build/libs/happybot-dev-all.jar
 
+WEBHOOK_URL=*censored*
+AVATAR_URL=https://cdn.discordapp.com/avatars/354736186516045835/ee232e232f4ae04198933d6d465a901a.png
+
 while true
 do
 java -jar HappyBot.jar
@@ -11,13 +14,17 @@ OUT=$?
 if [ ${OUT} -eq 10 ]
 then
 echo "[HappyBot] [Bash] Grabbing Update from Dropbox..."
+curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Downloading Dropbox Update..."}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
 wget -q -O HappyBot.jar ${DROPBOX_URL}
+curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Finished Dropbox Update!"}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
 echo "[HappyBot] [Bash] Update Done."
 fi
 if [ ${OUT} -eq 20 ]
 then
 echo "[HappyBot] [Bash] Grabbing Update from Jenkins..."
+curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Downloading Jenkins Update..."}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
 wget -q -O HappyBot.jar ${JENKINS_URL}
+curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Finished Jenkins Update!"}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
 echo "[HappyBot] [Bash] Update Done."
 fi
 if [ ${OUT} -eq 25 ]
