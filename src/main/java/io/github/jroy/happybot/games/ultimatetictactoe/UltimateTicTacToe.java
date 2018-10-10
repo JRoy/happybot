0b08ef36b40aa2aac809956ae3631f4cfeb466b2
okx-code <me@oliverallen.co.uk>
2018-10-10 16:48:32 +0100

    add ultimate tic tac toe

---
+++ src/main/java/io/github/jroy/happybot/games/ultimatetictactoe/UltimateTicTacToe.java
@@ -0,0 +1,4 @@
+package io.github.jroy.happybot.games;
+
+public class UltimateTicTacToe extends TwoPlayerReactionGame {
+}
---
+++ src/main/java/io/github/jroy/happybot/games/ultimatetictactoe/UltimateTicTacToeCommand.java
@@ -0,0 +1,4 @@
+package io.github.jroy.happybot.games.ultimatetictactoe;
+
+public class UltimateTicTacToeCommand {
+}
