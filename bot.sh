1b703d457b54371d3dd24dbaad5a7fd3a7210f84
JRoy <joshroy126@gmail.com>
2018-11-05 16:40:24 -0500

    Use git for script

--- bot.sh
+++ bot.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-DROPBOX_URL=*censored*
-JENKINS_URL=http://127.0.0.1:8080/job/happybot//lastSuccessfulBuild/artifact/build/libs/happybot-all.jar
-JENKINS_DEV_URL=http://127.0.0.1:8080/job/happybot-dev//lastSuccessfulBuild/artifact/build/libs/happybot-dev-all.jar
-
-WEBHOOK_URL=*censored*
-AVATAR_URL=https://cdn.discordapp.com/avatars/354736186516045835/ee232e232f4ae04198933d6d465a901a.png
-
 while true
 do
 java -Djava.util.logging.config.file=logging.properties -jar HappyBot.jar
 OUT=$?
 if [ ${OUT} -eq 10 ]
 then
-echo "[HappyBot] [Bash] Grabbing Update from Dropbox..."
-curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Downloading Dropbox Update..."}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
-wget -q -O HappyBot.jar ${DROPBOX_URL}
-curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Finished Dropbox Update!"}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
+echo "[HappyBot] [Bash] Grabbing Update from SSH..."
+cp -u tmp.jar HappyBot.jar
 echo "[HappyBot] [Bash] Update Done."
 fi
 if [ ${OUT} -eq 20 ]
 then
-echo "[HappyBot] [Bash] Grabbing Update from Jenkins..."
-curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Downloading Jenkins Update..."}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
-wget -q -O HappyBot.jar ${JENKINS_URL}
-curl -d '{"username": "happybot-bash", "avatar_url": "${AVATAR_URL}", "content": "Finished Jenkins Update!"}' -H "Content-Type: application/json" -X POST ${WEBHOOK_URL}
+echo "[HappyBot] [Bash] Grabbing Update from Git[dev]..."
+{
+git clone --single-branch -b dev https://github.com/JRoy/happybot.git
+cd happybot/
+./gradlew shadowJar
+cp build/libs/happybot-all.jar ../HappyBot.jar
+cd ..
+rm -r happybot/
+} &> /dev/null
 echo "[HappyBot] [Bash] Update Done."
 fi
 if [ ${OUT} -eq 25 ]
 then
-echo "[HappyBot] [Bash] Grabbing Update from Jenkins [DEV]..."
-wget -q -O HappyBot.jar ${JENKINS_DEV_URL}
+echo "[HappyBot] [Bash] Grabbing Update from Git[master]..."
+{
+git clone https://github.com/JRoy/happybot.git
+cd happybot/
+./gradlew shadowJar
+cp build/libs/happybot-all.jar ../HappyBot.jar
+cd ..
+rm -r happybot/
+} &> /dev/null
 echo "[HappyBot] [Bash] Update Done."
 fi
 echo "[HappyBot] [Bash] Rebooting in:"
