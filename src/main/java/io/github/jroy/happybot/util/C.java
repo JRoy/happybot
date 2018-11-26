cd981f15663fba409d7d587d619d6920e4bf18b8
okx-code <okx-code@users.noreply.github.com>
2018-11-26 17:37:52 +0000

    fix @JRoy not knowing how regexes work

--- src/main/java/io/github/jroy/happybot/util/C.java
+++ src/main/java/io/github/jroy/happybot/util/C.java
@@ -50,8 +50,7 @@ import java.util.regex.Pattern;
 @Slf4j
 public class C {
   private static String USER_AGENT = new UserAgent("happybot", "io.github.jroy", "v0.1", "wheezygold7931").toString();
-  private static final Pattern MENTION_REGEX = Pattern.compile("<@(\\d+)>");
-  private static final Pattern NOT_MENTION_REGEX = Pattern.compile("<@!(\\d+)>");
+  private static final Pattern MENTION_REGEX = Pattern.compile("<@!?(\\d+)>");
   private static final Map<TimeUnit, String> timeUnits = new LinkedHashMap<>();
 
   static {
@@ -67,15 +66,12 @@ public class C {
   public static Member matchMember(Member def, String string) {
     Guild guild = C.getGuild();
     Matcher matcher = MENTION_REGEX.matcher(string);
-    Matcher notMatcher = NOT_MENTION_REGEX.matcher(string);
     if(matcher.matches()) {
       return guild.getMemberById(matcher.group(1));
     }
-    if (notMatcher.matches()) {
-      return guild.getMemberById(notMatcher.group(1));
-    }
 
     Member closest = null;
+    // minimum similarity = 0.5 - higher requires more similarity
     double distance = 0.5;
 
     for (Member member : guild.getMembers()) {
