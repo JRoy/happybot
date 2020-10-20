79e635c111045c87710da70336ba96bb6d2a3ab2
Techno-coder <8334328+Techno-coder@users.noreply.github.com>
2020-10-20 15:32:18 +1300

    Fix whois for Technocoder (#232)
    Co-authored-by: Josh Roy <10731363+JRoy@users.noreply.github.com>

--- src/main/java/io/github/jroy/happybot/commands/WhoIsCommand.java
+++ src/main/java/io/github/jroy/happybot/commands/WhoIsCommand.java
@@ -19,6 +19,8 @@ import java.util.List;
 import java.util.Locale;
 
 public class WhoIsCommand extends CommandBase {
+  // Thanks Okx.
+  private static final String TECHNOCODER_ID = "180899128006410240";
 
   public WhoIsCommand() {
     super("whois", "[<user>]", "Gives you information about you or another user.", CommandCategory.GENERAL);
@@ -37,13 +39,25 @@ public class WhoIsCommand extends CommandBase {
     embed.setAuthor(C.getFullName(targetUser), null, targetUser.getAvatarUrl()).setThumbnail(targetUser.getAvatarUrl());
     embed.setDescription(targetMember.getAsMention()).setFooter("ID: " + targetUser.getId() + " • " + "Avatar ID: " + targetUser.getAvatarId(), null);
     embed.addField("Status", targetMember.getOnlineStatus().getKey(), true);
+
     ZonedDateTime joinDate = targetMember.getTimeJoined().atZoneSameInstant(ZoneId.of("America/New_York"));
+    if (targetUser.getId().equals(TECHNOCODER_ID)) {
+      joinDate = ZonedDateTime.of(2017, 3, 24, 5, 36, 0, 0, ZoneId.of("America/New_York"));
+    }
+
     embed.addField("Joined", joinDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US) + ", " + joinDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.US) + " " + joinDate.getDayOfMonth() + ", " + joinDate.getYear() + " " + joinDate.getHour() % 12 + ":" + joinDate.getMinute() + " " + ((joinDate.getHour() >= 12) ? "PM" : "AM"), true);
+
     List<Member> joinPosSort = new ArrayList<>(e.getGuild().getMembers());
     joinPosSort.sort(Comparator.comparing(Member::getTimeJoined));
-    embed.addField("Join Position", joinPosSort.indexOf(targetMember) + "/" + joinPosSort.size(), true);
+    int joinPosition = joinPosSort.indexOf(targetMember);
+    if (targetUser.getId().equals(TECHNOCODER_ID)) {
+      joinPosition = 39;
+    }
+    embed.addField("Join Position", joinPosition + "/" + joinPosSort.size(), true);
+
     ZonedDateTime registerDate = targetUser.getTimeCreated().atZoneSameInstant(ZoneId.of("America/New_York"));
     embed.addField("Registered", joinDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US) + ", " + registerDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.US) + " " + registerDate.getDayOfMonth() + ", " + registerDate.getYear() + " " + registerDate.getHour() % 12 + ":" + registerDate.getMinute() + " " + ((registerDate.getHour() >= 12) ? "PM" : "AM"), true);
+
     StringBuilder roles = new StringBuilder();
     int roleCount = 0;
     for (Role curRole : targetMember.getRoles()) {
