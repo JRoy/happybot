58537578b15b271555f6327cdbc5c9130efd9879
okx-code <okx@okx.sh>
2021-07-08 03:34:19 +0100

    Disable youtube api

--- src/main/java/io/github/jroy/happybot/Main.java
+++ src/main/java/io/github/jroy/happybot/Main.java
@@ -193,7 +193,7 @@ public class Main extends ListenerAdapter {
     apis.add(hypixel = new Hypixel(botConfig.getHypixelApiKey()));
     apis.add(new TwitterCentre(botConfig.getTwitterOauthKey(), botConfig.getTwitterOauthSecret(), botConfig.getTwitterAccessToken(), botConfig.getTwitterAccessTokenSecret()));
     apis.add(league = new League(botConfig.getRiotApiKey()));
-    apis.add(new YouTubeAPI(botConfig.getYoutubeApiKey()));
+    //apis.add(new YouTubeAPI(botConfig.getYoutubeApiKey()));
     log.info("Logging into APIs...");
     for (APIBase api : apis) {
       try {
